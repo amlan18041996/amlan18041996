2d5bb5dc808c5f4a7e42c728d172aacd5d1eb541
Amlan Sengupta <amlans3@gmail.com>
2025-04-06 03:24:51 +0530

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/amlan18041996',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
